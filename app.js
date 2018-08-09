18b774ed5979f6ab78815b2703ac0d152e9e2ee1
erickaugustor <erick.augustoramos@gmail.com>
2018-08-09 17:22:06 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+// import express from 'express';
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
