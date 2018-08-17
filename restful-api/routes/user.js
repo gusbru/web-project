6abbeb8e17722696ac0fb2f123854ca77e09df9f
erick.augustor <erick.augustoramos@gmail.com>
2018-08-17 08:35:32 -0300

    Refactoring

--- restful-api/routes/user.js
+++ restful-api/routes/user.js
@@ -2,14 +2,16 @@ import express from 'express';
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  req.orm.User
-    .findAll()
+routes.get('/', async (req, res) => {
+  const a = await req.orm.User.findAll();
+  res.send(a);
+/*
     .then(users => res.send(users))
     .catch((err) => {
       console.log(err);
       res.status(404).send({ error: 'Something failed!' });
     });
+*/
 });
 
 export default routes;
