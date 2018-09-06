d7ad6f9a6ad31c026d49105d7e9721f104cb755d
erick.augustor <erick.augustoramos@gmail.com>
2018-09-06 11:26:38 -0300

    Async/Await in Express

--- restful-api/models/User.js
+++ restful-api/models/User.js
@@ -1,14 +1,14 @@
 export default (sequelize, DataTypes) => (
-  sequelize.define('users', {
-    username: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    senha: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-  }, {
-      timestamps: false,
-  })
+	sequelize.define('users', {
+		username: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+		senha: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+	}, {
+		timestamps: false,
+	})
 );
--- restful-api/routes/user.js
+++ restful-api/routes/user.js
@@ -1,17 +1,21 @@
 import express from 'express';
 
+import wrapAsync from '../middlewares/wrapAsync';
+
 const routes = express.Router();
 
-routes.get('/carlos', async (req, res) => {
+routes.get('/', wrapAsync(async (req, res) => {
   const a = await req.orm.User.findAll();
   res.send(a);
+  
 /*
+  Promise way:
     .then(users => res.send(users))
     .catch((err) => {
       console.log(err);
       res.status(404).send({ error: 'Something failed!' });
     });
 */
-});
+}));
 
 export default routes;
