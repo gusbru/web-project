bfc47c83d3d92b7fcf3636a8e0601650b860ae09
erick.augustor <erick.augustoramos@gmail.com>
2018-11-01 20:40:10 -0300

    Scripts SQL

--- scripts/database-script.sql
+++ scripts/database-script.sql
@@ -1,44 +1,52 @@
-CREATE DATABASE project;
+CREATE DATABASE webproject;
 
-use project;
+use webproject;
 
 CREATE TABLE usuarios (
   login VARCHAR(255) NOT NULL,
   nome VARCHAR(255), 
   email VARCHAR(255) NOT NULL, 
   senha VARCHAR(255) NOT NULL, 
-  PRIMARY KEY (login),
+  PRIMARY KEY (login)
 );
-
 
 CREATE TABLE alunos (
+  codigo INT AUTO_INCREMENT,
   login VARCHAR(255),
   coeficiente_rendimento int,
-  PRIMARY KEY(login),
-  FOREIGN KEY (login) REFERENCES usuarios(cod)
+  PRIMARY KEY(codigo),
+  FOREIGN KEY (login) REFERENCES usuarios(login)
 );
 
-
 CREATE TABLE professores (
+  codigo INT AUTO_INCREMENT,
   login VARCHAR(255),
   area_de_formacao VARCHAR(255),
-  PRIMARY KEY(login),
-  FOREIGN KEY (login) REFERENCES usuario(cod) 
+  PRIMARY KEY(codigo),
+  FOREIGN KEY (login) REFERENCES usuarios(login) 
 );
 
 CREATE TABLE questoes (
 	codigo_questao INT,
   enunciado VARCHAR(255),
-  respota_correta VARCHAR(255),
-  alternativa VARCHAR(255),
+  respota_correta ENUM('A', 'B', 'C', 'D'),
   PRIMARY KEY (codigo_questao)
 );
 
+CREATE TABLE alternativas (
+  codigo_alternativa INT,
+  codigo_questao INT,
+  alternativa ENUM('A', 'B', 'C', 'D'),
+  descricao VARCHAR(255),
+  PRIMARY KEY (codigo_alternativa),
+  FOREIGN KEY (codigo_questao) REFERENCES questoes(codigo_questao)
+);
+
 CREATE TABLE alunos_questoes (
-  codigo INT,
+  codigo_alunos_questoes INT AUTO_INCREMENT,
 	login  VARCHAR(255),
-	cod_questao INT,
-  PRIMARY KEY (codigo),
-  FOREIGN KEY (login) REFERENCES ALUNO(login),
-  FOREIGN KEY (login) REFERENCES QUESTAO(cod_questao)
+	codigo_questao INT,
+  PRIMARY KEY (codigo_alunos_questoes),
+  FOREIGN KEY (login) REFERENCES usuarios(login),
+  FOREIGN KEY (codigo_questao) REFERENCES questoes(codigo_questao)
 );
---
+++ scripts/seed-script.sql
@@ -0,0 +1,14 @@
+
+INSERT INTO questoes VALUES (1, 'Quanto é 1+1?', 'A');
+INSERT INTO questoes VALUES (2, 'Quanto é 1-1?', 'D');
+
+
+INSERT INTO alternativas VALUES (1, 1, 'A', '2');
+INSERT INTO alternativas VALUES (2, 1, 'B', '1');
+INSERT INTO alternativas VALUES (3, 1, 'C', '0');
+INSERT INTO alternativas VALUES (4, 1, 'D', '6');
+
+INSERT INTO alternativas VALUES (5, 2, 'A', '-1');
+INSERT INTO alternativas VALUES (6, 2, 'B', '1');
+INSERT INTO alternativas VALUES (7, 2, 'C', '-2');
+INSERT INTO alternativas VALUES (8, 2, 'D', '0');
