dbeebbbf21ceb1281c28094a1b97b266e715a934
erick.augustor <erick.augustoramos@gmail.com>
2018-11-08 11:37:14 -0200

    Fixing the CASCADE in SQL

--- scripts/database-script.sql
+++ scripts/database-script.sql
@@ -14,7 +14,7 @@ CREATE TABLE alunos (
   login VARCHAR(255),
   coeficiente_rendimento int,
   PRIMARY KEY(codigo),
-  FOREIGN KEY (login) REFERENCES usuarios(login)
+  FOREIGN KEY (login) REFERENCES usuarios(login) ON DELETE CASCADE
 );
 
 CREATE TABLE professores (
@@ -22,7 +22,7 @@ CREATE TABLE professores (
   login VARCHAR(255),
   area_de_formacao VARCHAR(255),
   PRIMARY KEY(codigo),
-  FOREIGN KEY (login) REFERENCES usuarios(login) 
+  FOREIGN KEY (login) REFERENCES usuarios(login) ON DELETE CASCADE
 );
 
 CREATE TABLE questoes (
@@ -38,7 +38,56 @@ CREATE TABLE alternativas (
   alternativa ENUM('A', 'B', 'C', 'D'),
   descricao VARCHAR(255),
   PRIMARY KEY (codigo_alternativa),
-  FOREIGN KEY (codigo_questao) REFERENCES questoes(codigo_questao)
+  FOREIGN KEY (codigo_questao) REFERENCES questoes(codigo_questao) ON DELETE CASCADE
+);
+
+CREATE TABLE alunos_questoes (
+  codigo_alunos_questoes INT AUTO_INCREMENT,
+	login  VARCHAR(255),
+	codigo_questao INT,
+  resposta ENUM('A', 'B', 'C', 'D'),
+  PRIMARY KEY (codigo_alunos_questoes),
+  FOREIGN KEY (login) REFERENCES usuarios(login) CREATE DATABASE webproject;
+
+use webproject;
+
+CREATE TABLE usuarios (
+  login VARCHAR(255) NOT NULL,  
+  senha VARCHAR(255) NOT NULL, 
+  nome VARCHAR(255), 
+  PRIMARY KEY (login)
+);
+
+CREATE TABLE alunos (
+  codigo INT AUTO_INCREMENT,
+  login VARCHAR(255),
+  coeficiente_rendimento int,
+  PRIMARY KEY(codigo),
+  FOREIGN KEY (login) REFERENCES usuarios(login) ON DELETE CASCADE
+);
+
+CREATE TABLE professores (
+  codigo INT AUTO_INCREMENT,
+  login VARCHAR(255),
+  area_de_formacao VARCHAR(255),
+  PRIMARY KEY(codigo),
+  FOREIGN KEY (login) REFERENCES usuarios(login) ON DELETE CASCADE
+);
+
+CREATE TABLE questoes (
+	codigo_questao INT AUTO_INCREMENT,
+  enunciado VARCHAR(255),
+  resposta_correta ENUM('A', 'B', 'C', 'D'),
+  PRIMARY KEY (codigo_questao)
+);
+
+CREATE TABLE alternativas (
+  codigo_alternativa INT AUTO_INCREMENT,
+  codigo_questao INT,
+  alternativa ENUM('A', 'B', 'C', 'D'),
+  descricao VARCHAR(255),
+  PRIMARY KEY (codigo_alternativa),
+  FOREIGN KEY (codigo_questao) REFERENCES questoes(codigo_questao) ON DELETE CASCADE
 );
 
 CREATE TABLE alunos_questoes (
@@ -48,5 +97,6 @@ CREATE TABLE alunos_questoes (
   resposta ENUM('A', 'B', 'C', 'D'),
   PRIMARY KEY (codigo_alunos_questoes),
   FOREIGN KEY (login) REFERENCES usuarios(login),
-  FOREIGN KEY (codigo_questao) REFERENCES questoes(codigo_questao)
+  FOREIGN KEY (codigo_questao) REFERENCES questoes(codigo_questao) ON DELETE CASCADE,
+  FOREIGN KEY (codigo_questao) REFERENCES questoes(codigo_questao) ON DELETE CASCADE
 );
