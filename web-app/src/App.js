e27b7036ff0c1ec351638bcf0def16222f8383a1
erick.augustor <erick.augustoramos@gmail.com>
2018-11-17 16:45:11 -0200

    Axios and FETCH API

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -7,6 +7,20 @@ import Questions from './containers/Questions';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    this.callApi()
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
+
+  callApi = async () => {
+    const response = await fetch('locahost:3005/api/questoes');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
   render() {
     /*    
     const config = {
