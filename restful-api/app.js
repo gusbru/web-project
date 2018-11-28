5fd6b70ef78241c3bfbbb1b988843393659ad4aa
gusbru <gustavobruneto@gmail.com>
2018-11-28 18:24:07 -0200

    func: add x-auth-token to allow CORS

--- restful-api/app.js
+++ restful-api/app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 
 app.all('/api/*', function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With');
+	res.header('Access-Control-Allow-Headers', 'Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With, x-auth-token');
 	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST');
 	return next();
 });
