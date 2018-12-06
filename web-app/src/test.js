4e67664c61a35306b9e26bfed8182f1549a62843
erick.augustor <erick.augustoramos@gmail.com>
2018-12-05 23:47:05 -0200

    Selenium WEB driver

---
+++ web-app/src/test.js
@@ -0,0 +1,15 @@
+var webdriver = require('selenium-webdriver');
+var chrome = require('selenium-webdriver/chrome');
+var path = require('chromedriver').path;
+
+var service = new chrome.ServiceBuilder(path).build();
+chrome.setDefaultService(service);
+
+var driver = new webdriver.Builder()
+    .withCapabilities(webdriver.Capabilities.chrome())
+    .build();
+
+driver.get("http://localhost:3000/");
+
+
+driver.close();
